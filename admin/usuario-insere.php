bfb9d49fb779863c55097834b52ac1b3da2a365a
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-08-10 11:01:56 -0300

    function insere
    preparing

---
+++ admin/usuario-insere.php
@@ -0,0 +1,72 @@
+<?php
+use Projeto\Usuario;
+require_once "../vendor/autoload.php";
+
+if (isset($_POST['inserir'])) {
+	$usuario = new Usuario;
+	$usuario->setNome($_POST['nome']);
+	$usuario->setEmail($_POST['email']);
+    $usuario->setSenha($_POST['senha']);
+    $listaSenac = $usuario->getSenac();
+    $usuario->setSenac($_POST['senac']);
+	$usuario->inserir();
+	header("location:usuarios.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../visualizacoes/bootstrap.css">
+    <title>Inserir Usuário</title>
+</head>
+<body>
+    <section class="row justify-content-center py-4">
+    <article class="col-8 bg-white rounded shadow my-1 py-4">
+		
+		<h2 class="text-center">
+		Inserir novo usuário
+		</h2>
+				
+		<form class="mx-auto w-75" action="" method="post" id="form-inserir" name="form-inserir">
+
+			<div class="mb-3">
+				<label class="form-label" for="nome">Nome:</label>
+				<input class="form-control" type="text" id="nome" name="nome" required>
+			</div>
+
+			<div class="mb-3">
+				<label class="form-label" for="email">E-mail:</label>
+				<input class="form-control" type="email" id="email" name="email" required>
+			</div>
+
+			<div class="mb-3">
+				<label class="form-label" for="senha">Senha:</label>
+				<input class="form-control" type="password" id="senha" name="senha" required>
+			</div>
+
+			<div class="mb-3">
+				<label class="form-label" for="tipo">Senac:</label>
+				<select class="form-select form-control" name="tipo" id="tipo" required>
+					<option value=""></option>
+                    <?php
+                    foreach ($listaSenac as $senac) {
+                    ?>
+                    <option value=""><?=$usuario->getSenac()?></option>
+                    <?php
+                    }
+                    ?>
+                    
+				</select>
+			</div>
+                <button class="btn btn-success  col" id="inserir" name="inserir"><i class="bi bi-save"></i> Inserir Usuário</button>
+                <button class="btn btn-outline-danger  col" id="cancelar" name="cancelar"><i class="bi bi-save"></i> Cancelar</button>
+		</form>
+		
+	</article>
+    </section>
+</body>
+</html>
